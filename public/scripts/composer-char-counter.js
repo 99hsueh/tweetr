d5ac3eb6eb44922f100f4de86168c31b0d924849
99hsueh <99hhsueh@gmail.com>
2017-02-21 22:27:20 +0000

    added dynamic counter

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,23 @@
+$( document).ready(function() {
+  var textBox = document.querySelector('textarea');
+  var maxLength = 140;
+  textBox.addEventListener('input', function(event){
+  var charRemaining = maxLength - this.value.length;
+  var counterClass = this.parentElement.getElementsByClassName('counter');
+  $(counterClass).html(charRemaining);
+    if (charRemaining < 0){
+      $(counterClass).addClass('counter-red');
+    } else {
+      $(counterClass).removeClass('counter-red');
+    }
+  })
+});
+
+
+// console.log(this);
+// console.log(event.target);
+
+// TRAVERSE PARENT THEN CHILD
+// this.parentElement.getElementsByClassName('counter')[0]
+// IN JQUERY
+// $(this).parent().children('.counter')
